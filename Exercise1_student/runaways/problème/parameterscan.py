2a590f2c26af7fe21d47f48eadebd1750eb5ba9c
timothee420 <timotheebal@gmail.com>
2026-03-09 14:41:55 +0100

    ed

--- Exercise1_student/runaways/problème/parameterscan.py
+++ Exercise1_student/runaways/problème/parameterscan.py
@@ -15,13 +15,8 @@ N0 = 0.0
 g = 0.5  
 d = 0.01
 
-<<<<<<< Updated upstream
-alpha =  0.5  # 1 explicit, 0 implicit, 0.5 semi-implicit
-question = 'a'
-=======
 alpha = 0  # 1 explicit, 0 implicit, 0.5 semi-implicit
 question = 'a' # change this to 'b', 'c', 'd', or 'e' for the different questions, default is 'a'
->>>>>>> Stashed changes
 
 
 dt = tf / 2**np.arange(2,8) #sera changé ci-dessous si nécessaire pour les questions b) et d)
